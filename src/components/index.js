4811d97fef999124c9fdd568887fa63aa11cdec2
rafagabidulin <rafagabidulin@gmail.com>
2022-04-28 22:26:28 +0300

    created SortPopup component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Button from "./Button.jsx";
 import Header from "./Header.jsx";
 import Categories from "./Categories.jsx";
+import SortPopup from "./SortPopup.jsx";
 
 export {
   Button,
   Header,
-  Categories
+  Categories,
+  SortPopup
 }
