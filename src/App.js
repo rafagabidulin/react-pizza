101b39b1a21f5d9e38d62518969ab637a30e4df1
rafagabidulin <rafagabidulin@gmail.com>
2022-05-03 18:43:01 +0300

    changed fetch to axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import { Header } from './components/index.js';
 import { Home, Cart } from './pages/index.js';
 
@@ -7,8 +8,8 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
-      setPizzas(json.pizzas);
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
     });
   }, []);
 
