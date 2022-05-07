c89d3c6100e447b062a4e3a1fe533e4a3815a474
rafagabidulin <rafagabidulin@gmail.com>
2022-05-07 23:11:01 +0300

    changed filters from string type to object type

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,13 @@ function Home({ items }) {
           onClick={(name) => console.log(name)}
           items={['Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые']}
         />
-        <SortPopup items={['популярности', 'цене', 'алфавиту']} />
+        <SortPopup
+          items={[
+            { name: 'популярности', type: 'popular' },
+            { name: 'цене', type: 'price' },
+            { name: 'алфавиту', type: 'alphabet' },
+          ]}
+        />
       </div>
       <h2 className='content__title'>Все пиццы</h2>
       <div className='content__items'>
